2ed93183e98bf03da7db2e6431d67bcafc0cbd0b
Jucelino Santos <jucelino-ss@hotmail.com>
2022-10-29 13:05:45 -0300

    Tratamento de token expirado

--- src/main/java/br/com/univesp/mercadocell/mercadocell/controller/exception/ControllerExceptionHandler.java
+++ src/main/java/br/com/univesp/mercadocell/mercadocell/controller/exception/ControllerExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.multipart.MultipartException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.Instant;
@@ -72,6 +73,17 @@ public class ControllerExceptionHandler {
         err.setTimestamp(Instant.now());
         err.setStatus(HttpStatus.BAD_REQUEST.value());
         err.setError("Requisição de imagem inválida!");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+    }
+
+    @ExceptionHandler(TokenExpiredException.class)
+    public ResponseEntity<StandardError> expiredTokenException(TokenExpiredException e, HttpServletRequest request){
+        StandardError err = new StandardError();
+        err.setTimestamp(Instant.now());
+        err.setStatus(HttpStatus.BAD_REQUEST.value());
+        err.setError("Token expirado!");
         err.setMessage(e.getMessage());
         err.setPath(request.getRequestURI());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
