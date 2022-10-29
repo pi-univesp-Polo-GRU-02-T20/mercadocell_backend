a69220b3b620d45bade615bd9a761fa29187eff6
Vinicius V Santos <vinicius.v.sts@gmail.com>
2022-10-29 11:46:39 -0300

    Authorization Bearer corrigido na interface do swagger

--- src/main/java/br/com/univesp/mercadocell/mercadocell/config/swagger/SpringFoxConfig.java
+++ src/main/java/br/com/univesp/mercadocell/mercadocell/config/swagger/SpringFoxConfig.java
@@ -4,47 +4,38 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.OAuthBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.*;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.HttpAuthenticationScheme;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Configuration
 public class SpringFoxConfig {
     @Bean
     public Docket docket() {
+
+        HttpAuthenticationScheme authenticationScheme = HttpAuthenticationScheme.JWT_BEARER_BUILDER
+                .name("Authorization")
+                .build();
+
         return new Docket(DocumentationType.OAS_30)
                 .apiInfo(new ApiInfoBuilder()
                         .title("MercadoCell API")
                         .build())
-    //          .securitySchemes(Arrays.asList(securityScheme()))
                 .securityContexts(Arrays.asList(securityContext()))
-                .securitySchemes(Arrays.asList(apiKey()))
+                .securitySchemes(Collections.singletonList(authenticationScheme))
                 .select().apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .build();
     }
 
-    private SecurityScheme securityScheme(){
-        return new OAuthBuilder()
-                .name("Mercadocell")
-                .grantTypes(grantTypes())
-                .scopes(scopes())
-                .build();
-    }
-    private List<GrantType> grantTypes(){
-        return Arrays.asList(new ResourceOwnerPasswordCredentialsGrant("/oauth/token"));
-    }
-
-    private List<AuthorizationScope> scopes(){
-        return Arrays.asList(new AuthorizationScope("READ", "Acesso de leitura"),
-                new AuthorizationScope("WRITE", "Acesso de edição")
-        );
-    }
 
     private ApiKey apiKey() {
         return new ApiKey("JWT", "Authorization", "header");
@@ -58,6 +49,6 @@ public class SpringFoxConfig {
         AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
         AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
         authorizationScopes[0] = authorizationScope;
-        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+        return Arrays.asList(new SecurityReference("Authorization", authorizationScopes));
     }
 }
