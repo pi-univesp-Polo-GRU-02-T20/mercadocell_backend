15ab773ad85a2003829932b12c500524cb5cf4f7
Jucelino Santos <jucelino-ss@hotmail.com>
2022-11-02 16:19:04 -0300

    Retorno de token JWT em JSON

---
+++ src/main/java/br/com/univesp/mercadocell/mercadocell/dto/ResponseTokenJWTDTO.java
@@ -0,0 +1,13 @@
+package br.com.univesp.mercadocell.mercadocell.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ResponseTokenJWTDTO {
+
+    private String jwtToken;
+
+
+}
