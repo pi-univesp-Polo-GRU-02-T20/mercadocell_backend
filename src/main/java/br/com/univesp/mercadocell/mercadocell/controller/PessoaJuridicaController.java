66fa87a6e29827e79fa3933c6229d801f1e501d4
micheldigui <michelssouza@hotmail.com>
2022-11-06 13:05:45 -0300

    Ativacao cadastro endereco pessoa juridica

--- src/main/java/br/com/univesp/mercadocell/mercadocell/controller/PessoaJuridicaController.java
+++ src/main/java/br/com/univesp/mercadocell/mercadocell/controller/PessoaJuridicaController.java
@@ -1,5 +1,7 @@
 package br.com.univesp.mercadocell.mercadocell.controller;
 
+import br.com.univesp.mercadocell.mercadocell.dto.PessoaJuridicaDTO;
+import br.com.univesp.mercadocell.mercadocell.model.Pessoa;
 import br.com.univesp.mercadocell.mercadocell.model.PessoaJuridica;
 import br.com.univesp.mercadocell.mercadocell.service.PessoaJuridicaService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -24,20 +27,36 @@ public class PessoaJuridicaController {
     }
 
     @GetMapping(path="/{idPessoaJuridica}")
-    public ResponseEntity<PessoaJuridica> buscarPessoaJuridicaPorId(@PathVariable int idPessoaJuridica) {
-        PessoaJuridica pessoaJuridica  = pessoaJuridicaService.buscarPessoaJuridicaPorId(idPessoaJuridica);
-        return ResponseEntity.ok().body(pessoaJuridica);
+    public ResponseEntity<PessoaJuridicaDTO> buscarPessoaJuridicaPorId(@PathVariable int idPessoaJuridica) {
+        PessoaJuridicaDTO pessoaJuridicaDTO = convertePessoaJuridicaParaPessoaJuridicaDTO(
+                pessoaJuridicaService.buscarPessoaJuridicaPorId(idPessoaJuridica)
+        );
+        return ResponseEntity.ok().body(pessoaJuridicaDTO);
     }
 
     @GetMapping(path="/buscar/{nomePessoaJuridica}")
-    public ResponseEntity<PessoaJuridica> buscarPessoaJuridicaPorNome(@PathVariable String nomePessoaJuridica) {
-        PessoaJuridica pessoaJuridica = pessoaJuridicaService.buscarPessoaJuridicaPorNome(nomePessoaJuridica);
-        return ResponseEntity.ok().body(pessoaJuridica);
+    public ResponseEntity<PessoaJuridicaDTO> buscarPessoaJuridicaPorNome(@PathVariable String nomePessoaJuridica) {
+        PessoaJuridicaDTO pessoaJuridicaDTO = convertePessoaJuridicaParaPessoaJuridicaDTO(
+                pessoaJuridicaService.buscarPessoaJuridicaPorNome(nomePessoaJuridica)
+                );
+        return ResponseEntity.ok().body(pessoaJuridicaDTO);
     }
 
     @GetMapping
-    public List<PessoaJuridica> listarPessoasJuridicas() {
-        return pessoaJuridicaService.listarPessoasJuridicas();
+    public List<PessoaJuridicaDTO> listarPessoasJuridicas() {
+        List<PessoaJuridicaDTO> listaPessoasJuridicasDTO = new ArrayList<>();
+            for(PessoaJuridica pessoaJuridica : pessoaJuridicaService.listarPessoasJuridicas()){
+                listaPessoasJuridicasDTO.add(convertePessoaJuridicaParaPessoaJuridicaDTO(pessoaJuridica));
+        }
+        return listaPessoasJuridicasDTO;
+    }
+
+    private PessoaJuridicaDTO convertePessoaJuridicaParaPessoaJuridicaDTO(PessoaJuridica pessoa) {
+        return new PessoaJuridicaDTO(
+                pessoa.getCodPessoa(),
+                pessoa.getNomePessoa()
+
+        );
     }
 
     @PutMapping
--- src/main/java/br/com/univesp/mercadocell/mercadocell/dto/PessoaJuridicaDTO.java
+++ src/main/java/br/com/univesp/mercadocell/mercadocell/dto/PessoaJuridicaDTO.java
@@ -1,14 +1,24 @@
 package br.com.univesp.mercadocell.mercadocell.dto;
 
 import br.com.univesp.mercadocell.mercadocell.model.Pessoa;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import javax.annotation.sql.DataSourceDefinition;
+import java.time.LocalDate;
 
 
 @Data
+@AllArgsConstructor
 public class PessoaJuridicaDTO extends Pessoa {
     private String nomeRazaoSocial;
     private String codCNPJ;
 
+
+    public PessoaJuridicaDTO(Integer codPessoa, String nomePessoa) {
+        this.codPessoa = codPessoa;
+        this.nomePessoa = nomePessoa;
+
+
+    }
 }
