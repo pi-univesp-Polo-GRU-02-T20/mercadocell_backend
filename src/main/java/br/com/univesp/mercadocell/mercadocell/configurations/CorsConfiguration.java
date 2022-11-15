ec1b57922cfcd1101360242a35b410c32e2e2675
Jucelino Santos <jucelino-ss@hotmail.com>
2022-11-15 16:32:56 -0300

    Ajustes CORS

--- src/main/java/br/com/univesp/mercadocell/mercadocell/configurations/CorsConfiguration.java
+++ src/main/java/br/com/univesp/mercadocell/mercadocell/configurations/CorsConfiguration.java
@@ -15,9 +15,9 @@ public class CorsConfiguration implements WebMvcConfigurer {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("*")
-                        .allowedHeaders("*")
-                       // .allowCredentials( true )
-                        .allowedMethods("GET", "POST", "PUT", "DELETE");//, "OPTIONS",  "HEAD", "TRACE", "CONNECT"
+                        .allowedHeaders("Authorization")
+                      //  .allowCredentials( true )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");//, ,  "HEAD", "TRACE", "CONNECT"
             }
         };
     }
